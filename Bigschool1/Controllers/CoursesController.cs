d839191a801d6f9d15e72968c5693f28ab87da22
vituo <vituo@BOYKINGLZ>
2022-06-03 10:54:07 +0700

    Add project files.

---
+++ Bigschool1/Controllers/CoursesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bigschool1.Controllers
+{
+    public class CoursesController : Controller
+    {
+        // GET: Courses
+        public ActionResult Creste()
+        {
+            return View();
+        }
+    }
+}
